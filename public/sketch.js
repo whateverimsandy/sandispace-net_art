c001ee4da4f21acabd64a1f51b9f2801bd896d29
SandyVu <126855585+whateverimsandy@users.noreply.github.com>
2023-03-24 17:00:50 +1100

    Initial commit

---
+++ public/sketch.js
@@ -0,0 +1,10 @@
+document.body.style.margin   = 0
+document.body.style.overflow = `hidden`
+
+function setup () {
+   createCanvas (innerWidth, innerHeight)
+}
+
+function  draw () {
+   background (`turquoise`)
+}
