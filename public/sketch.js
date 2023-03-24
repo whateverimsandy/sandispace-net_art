5abfbc089293ea688627f013c919f1d4e9114c9f
SandyVu <sandyvu1311@gmail.com>
2023-03-24 18:16:11 +1100

    anything changes

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,28 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
+let x= 0;
+
 function setup () {
    createCanvas (innerWidth, innerHeight)
+   background (`black`)
 }
 
 function  draw () {
-   background (`turquoise`)
+   
+   noStroke()
+   fill (255, random(255),random(255))
+   ellipse (mouseX,mouseY,100,100)
+
+   myT();
+   
 }
+
+function myT(){
+   fill('black')
+   textAlign('CENTER')
+   textSize(200)
+   text("lololololololo",300,300)
+   
+}
+
